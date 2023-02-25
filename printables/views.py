aa0f5564a16857ef25a49a754ea3796d53aeb26c
Ne0sky <mailmeakash2@gmail.com>
2023-02-25 15:50:39 +0530

    origin

--- printables/views.py
+++ printables/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
+def products(request):
+    return render(request, 'products.html')
+
 def home(request):
     return render(request, 'home.html')
